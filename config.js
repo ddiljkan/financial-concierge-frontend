85e68a3455d9265b2187ffccdc57fd846410cf99
Dejan Diljkan <dejan.diljkan@outlook.com>
2026-04-13 14:42:34 +0200

    introduce config file

---
+++ config.js
@@ -0,0 +1,6 @@
+window.APP_CONFIG = {
+  API_BASE_URL: 'https://c95kxwskni.execute-api.eu-central-1.amazonaws.com/',
+  UPLOAD_PATH: '/upload-url',
+  AWS_REGION: 'eu-central-1',
+  APP_NAME: 'Financial Concierge'
+};
