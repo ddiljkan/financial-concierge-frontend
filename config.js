19f14707ed030d0c730cf464f189a209402ed550
maksim-inic <maksim.inic@outlook.com>
2026-04-28 21:15:04 +0200

    Add api on gateway path

--- config.js
+++ config.js
@@ -1,6 +1,6 @@
 window.APP_CONFIG = {
   API_BASE_URL: 'https://5ncyd33po2.execute-api.eu-central-1.amazonaws.com',
-  UPLOAD_PATH: '/upload-url',
+  UPLOAD_PATH: '/api/upload-url',
   AWS_REGION: 'eu-central-1',
   APP_NAME: 'Financial Concierge'
 };
