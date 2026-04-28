9c9183ac0fd7fd03cbce2131702f91aec29dbeb8
maksim-inic <maksim.inic@outlook.com>
2026-04-28 21:06:58 +0200

    fix upload

--- config.js
+++ config.js
@@ -1,5 +1,5 @@
 window.APP_CONFIG = {
-  API_BASE_URL: 'https://c95kxwskni.execute-api.eu-central-1.amazonaws.com/',
+  API_BASE_URL: 'https://5ncyd33po2.execute-api.eu-central-1.amazonaws.com',
   UPLOAD_PATH: '/upload-url',
   AWS_REGION: 'eu-central-1',
   APP_NAME: 'Financial Concierge'
