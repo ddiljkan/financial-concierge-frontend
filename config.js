b697811c469f9e5eaa996fd5e9b182277f9d74e0
Dejan Diljkan <dejan.diljkan@outlook.com>
2026-04-29 20:17:41 +0200

    alter api_base_url for altered api endpoint

--- config.js
+++ config.js
@@ -1,5 +1,5 @@
 window.APP_CONFIG = {
-  API_BASE_URL: 'https://5ncyd33po2.execute-api.eu-central-1.amazonaws.com',
+  API_BASE_URL: 'https://so9eexsby5.execute-api.eu-central-1.amazonaws.com',
   UPLOAD_PATH: '/api/upload-url',
   AWS_REGION: 'eu-central-1',
   APP_NAME: 'Financial Concierge'
