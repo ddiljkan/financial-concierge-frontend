a64d774816da8c9b28bf6495802283a7136af87e
maksim-inic <maksim.inic@outlook.com>
2026-06-09 19:34:18 +0200

    adapt base URL

--- config.js
+++ config.js
@@ -1,5 +1,5 @@
 window.APP_CONFIG = {
-  API_BASE_URL: 'https://e4wl6mav54.execute-api.eu-central-1.amazonaws.com/',
+  API_BASE_URL: 'https://itu6gw6x10.execute-api.eu-central-1.amazonaws.com/',
   UPLOAD_PATH: '/api/upload-url',
   AWS_REGION: 'eu-central-1',
   APP_NAME: 'Financial Concierge'
